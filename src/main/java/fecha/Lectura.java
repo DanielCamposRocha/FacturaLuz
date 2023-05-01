845b9df3410c401c8c9cb285c6ccc61af0532241
Daniel <danielcamposrocha9@gmail.com>
2023-05-01 19:52:49 +0200

    Lee los datos desde el excell del contador calcula media de consumos total

---
+++ src/main/java/fecha/Lectura.java
@@ -0,0 +1,55 @@
+package fecha;
+
+import java.time.LocalDateTime;
+
+public class Lectura {
+    private String numContador;
+    private LocalDateTime fechaContador;
+
+    private Double consumo;
+    private String metodoObtencion;
+
+    public Lectura(String numContador, LocalDateTime fechaContador, Double consumo, String metodoObtencion) {
+        this.numContador = numContador;
+        this.fechaContador = fechaContador;
+        this.consumo = consumo;
+        this.metodoObtencion = metodoObtencion;
+    }
+
+    public String getNumContador() {
+        return numContador;
+    }
+
+    public void setNumContador(String numContador) {
+        this.numContador = numContador;
+    }
+
+    public LocalDateTime getFechaContador() {
+        return fechaContador;
+    }
+
+    public void setFechaContador(LocalDateTime fechaContador) {
+        this.fechaContador = fechaContador;
+    }
+
+    public Double getConsumo() {
+        return consumo;
+    }
+
+    public void setConsumo(Double consumo) {
+        this.consumo = consumo;
+    }
+
+    public String getMetodoObtencion() {
+        return metodoObtencion;
+    }
+
+    public void setMetodoObtencion(String metodoObtencion) {
+        this.metodoObtencion = metodoObtencion;
+    }
+
+    @Override
+    public String toString() {
+        return numContador +" "+fechaContador + " " + consumo +" "+ metodoObtencion ;
+    }
+}
