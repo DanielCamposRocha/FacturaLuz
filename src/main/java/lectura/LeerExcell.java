08930a7279837c913b9f7b6d361e12920ea66ed2
DanielCamposRocha <114098301+DanielCamposRocha@users.noreply.github.com>
2023-05-01 21:01:42 +0200

    Update LeerExcell.java

--- src/main/java/lectura/LeerExcell.java
+++ src/main/java/lectura/LeerExcell.java
@@ -1,13 +1,13 @@
-package lectura;
+package fecha;
 
 
-import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 
@@ -17,6 +17,31 @@ public class LeerExcell {
         listaLecturas=new ArrayList<>();
         leerExcell();
         calcularMedia();
+        mediaFiltrada();
+    }
+
+    private static void mediaFiltrada() {
+        ArrayList <Lectura> lista21=new ArrayList<>();
+        ArrayList <Lectura> lista22=new ArrayList<>();
+        ArrayList <Lectura> lista23=new ArrayList<>();
+        double consumo21=0;
+        double consumo22=0;
+        double consumo23=0;
+        for (Lectura lectura:listaLecturas
+        ) {
+            if(lectura.getFechaContador().getYear()==2021){
+                lista21.add(lectura);
+                consumo21=consumo21+lectura.getConsumo();}
+            if(lectura.getFechaContador().getYear()==2022){
+                lista22.add(lectura);
+                consumo22=consumo22+lectura.getConsumo();}
+            if(lectura.getFechaContador().getYear()==2023){
+                lista23.add(lectura);
+                consumo23=consumo23+lectura.getConsumo();}
+        }
+        System.out.println("Por lo que el consumo medio en 2021 ha sido de: "+(consumo21/ lista21.size())+" kw/h");
+        System.out.println("Por lo que el consumo medio en 2022 ha sido de: "+(consumo22/ lista22.size())+" kw/h");
+        System.out.println("Por lo que el consumo medio en 2023 ha sido de: "+(consumo23/ lista23.size())+" kw/h");
     }
 
     private static void calcularMedia() {
@@ -32,7 +57,7 @@ public class LeerExcell {
     public static void leerExcell(){
 
         try {
-            FileInputStream fis=new FileInputStream("consumptions.xlsx");
+            FileInputStream fis=new FileInputStream("Lecturas264.xlsx");
             XSSFWorkbook libro = new XSSFWorkbook(fis);
             XSSFSheet hoja = libro.getSheetAt(0);
             int contadorFilas=0;
@@ -44,8 +69,9 @@ public class LeerExcell {
                     Double hora=fila.getCell(2).getNumericCellValue();
                     String consumo=fila.getCell(3).getStringCellValue();
                     String metodoObtencion=fila.getCell(4).getStringCellValue();
+                    LocalDateTime fechL=pasarLectura(fecha,hora);
                     Double lectura=Double.parseDouble(consumo.split(",")[0]+"."+consumo.split(",")[1]);
-                    listaLecturas.add(new Lectura(numContador,fecha,hora,lectura,metodoObtencion));
+                    listaLecturas.add(new Lectura(numContador,fechL,lectura,metodoObtencion));
                              }
                 contadorFilas++;
             }
@@ -55,6 +81,14 @@ public class LeerExcell {
             System.out.println("Error al leer el archivo");
             e.printStackTrace();
         }
-        System.out.println(listaLecturas);
+
+    }
+
+    public static LocalDateTime pasarLectura(String fecha,Double hora){
+        int horita= (int) Math.round(hora-1);
+        int dia=Integer.parseInt(fecha.split("/")[0]);
+        int mes=Integer.parseInt(fecha.split("/")[1]);
+        int anho=Integer.parseInt(fecha.split("/")[2]);
+        return LocalDateTime.of(anho,mes,dia,horita,0);
     }
 }
